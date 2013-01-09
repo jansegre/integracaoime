346382a0a9e334bcf69511eab43095f0304da79d
Jan Segre <jan@segre.in>
2013-01-09 11:47:55 -0200

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IntegracaoIME::Application.config.secret_token = '5091bbca5e9e65535510e5e0cffb810ddb068ba6a5355f63a3a2d229ca0e3892132c4dbae5fe35c85777baa7ec36f51726d2a81c6ec6e5c756ada7ff011b126d'

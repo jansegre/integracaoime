232280207fd208a0a27dfe173995eeb8df4a6f2d
Jan Segre <jan@segre.in>
2013-01-09 23:02:09 -0200

    Store sessions on database.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-IntegracaoIME::Application.config.session_store :cookie_store, key: '_IntegracaoIME_session'
+# IntegracaoIME::Application.config.session_store :cookie_store, key: '_IntegracaoIME_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
-# IntegracaoIME::Application.config.session_store :active_record_store
+IntegracaoIME::Application.config.session_store :active_record_store

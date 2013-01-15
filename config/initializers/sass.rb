3395ccc385e0c04418eee746504ff39fbe9ad740
Jan Segre <jan@segre.in>
2013-01-15 21:32:18 -0200

    Safer path calculation.

--- config/initializers/sass.rb
+++ config/initializers/sass.rb
@@ -1,3 +1,3 @@
 Sass::Engine::DEFAULT_OPTIONS[:load_paths].tap do |load_paths|
-  load_paths << "#{Rails.root}/app/assets/stylesheets"
+  load_paths << Rails.root.join('app/assets/stylesheets')
 end

dffcf4dac72b9251359261e3791583e8d3c9af23
Jan Segre <jan@segre.in>
2013-01-17 21:06:14 -0200

    Adding uncertain production action mailer config. Test it!

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,4 +68,11 @@ IntegracaoIME::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # Set some mailer defaults
+  #TODO change these after staging fase
+  config.action_mailer.default_url_options = {:host => 'integracaoime.segre.in'}
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
 end

df7164d5be19fcd4ed4b3656f83795f479127997
Jan Segre <jan@segre.in>
2013-01-17 12:05:45 -0200

    Adding admin user and admin dashboard.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 IntegracaoIME::Application.routes.draw do
+  ActiveAdmin.routes(self)
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+
   devise_for :users
 
   root :to => 'welcome#index'

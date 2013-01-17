90781065d0c4691d3ad59b06e1ada5aafb0bd016
Jan Segre <jan@segre.in>
2013-01-17 13:23:58 -0200

    Improving login and overrall layout.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ IntegracaoIME::Application.routes.draw do
 
   devise_for :admin_users, ActiveAdmin::Devise.config
 
-  devise_for :users
+  devise_for :users, :skip => [:sessions]
+  as :user do
+    get 'login' => 'devise/sessions#new', :as => :new_user_session
+    post 'login' => 'devise/sessions#create', :as => :user_session
+    delete 'logout' => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
 
   root :to => 'welcome#index'
 

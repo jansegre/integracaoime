cadd40384a8992e381b616dffde9c1761c5375f6
Jan Segre <jan@segre.in>
2013-01-28 02:04:52 -0200

    Hiding sign_in_at columns on admin.

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -10,8 +10,8 @@ ActiveAdmin.register User do
     column :email
     bcolumn :admin
     bcolumn :approved
-    column :current_sign_in_at
-    column :last_sign_in_at
+    #column :current_sign_in_at
+    #column :last_sign_in_at
     column :sign_in_count
     default_actions
   end

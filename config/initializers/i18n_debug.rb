bed67752f777e1d54ccf36e33f37d757149717c5
Jan Segre <jan@segre.in>
2013-01-30 11:44:30 -0200

    Adding i18n debug tool.

---
+++ config/initializers/i18n_debug.rb
@@ -0,0 +1,15 @@
+require 'i18n'
+debug_i18n = false
+
+if (Rails.env.development? || Rails.env.test?) && debug_i18n
+  module I18n
+    class << self
+      def translate_with_debug(*args)
+        translation = translate_without_debug(*args)
+        Rails.logger.debug "  I18N: #{translation}  #{args.map{ |a| a.inspect }.join(", ")}"
+        translation
+      end
+      alias_method_chain :translate, :debug
+    end
+  end
+end

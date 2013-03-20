b59024daffdeeb90052a6a469542ae0df85d2b69
Jan Segre <jan@segre.in>
2013-03-20 13:23:41 -0300

    Making sponsor cells look like cards.

--- app/uploaders/hint_image_uploader.rb
+++ app/uploaders/hint_image_uploader.rb
@@ -43,7 +43,7 @@ class HintImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process resize_to_fill: [140, 140]
+    process resize_to_fill: [275, 200]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.

7dc327e6a015d107418961a7a2f365ac8229c0ba
Jan Segre <jan@segre.in>
2013-04-30 15:34:43 -0300

    Updating mongoid and fixing students_controller.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,19 +13,27 @@ class StudentsController < ApplicationController
 
   # recive a post and create that resume
   def create_resume
-    if current_user.create_resume(params[:resume])
+    @url = { action: "create_resume" }
+    @resume = current_user.create_resume
+    if @resume.update_attributes(params[:resume])
       redirect_to({ action: "resume" }, notice: t("flash.resume_created"))
     else
-      render action: "new_resume"
+      flash[:notice] = t("flash.resume_updated")
+      render action: "resume"
     end
   end
 
   # recive a put and update a resume
   def update_resume
-    if current_user.resume.update_attributes(params[:resume])
+    @resume = current_user.resume
+    @url = { action: "update_resume" }
+    #@resume.assign_attributes(params[:resume])
+    #if @resume.valid?
+    if @resume.update_attributes(params[:resume])
       redirect_to({ action: "resume" }, notice: t("flash.resume_updated"))
     else
-      render action: "edit_resume"
+      flash[:notice] = t("flash.resume_updated")
+      render action: "resume"
     end
   end
 

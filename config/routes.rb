5e8b51f464daad27588924e104f53af203613d16
Jan Segre <jan@segre.in>
2013-05-20 05:11:03 -0300

    Reset password through email.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 IntegracaoIME::Application.routes.draw do
 
-  root :to => "welcome#index"
+  root :to => "welcome#index", as: :new_user_session
 
   get "page/:slug" => "pages#show", :as => :page
 
@@ -28,6 +28,7 @@ IntegracaoIME::Application.routes.draw do
 
   devise_scope :user do
     #get "login" => "devise/sessions#new", :as => :new_user_session
+    get "login" => "welcome#index", :as => :new_user_session
     post "login" => "devise/sessions#create", :as => :user_session
     delete "logout" => "devise/sessions#destroy", :as => :destroy_user_session
 

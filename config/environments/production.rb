5f1cb9b3a73b6abeb27c43ef689872e118a09eb3
Jan Segre <jan@segre.in>
2013-06-13 10:33:22 -0300

    Fixing problem when sending email with exim.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,8 @@ IntegracaoIME::Application.configure do
 
   # Set some mailer defaults
   #TODO change these after staging fase
-  config.action_mailer.default_url_options = {:host => 'integracaoime.com.br'}
+  config.action_mailer.default_url_options = {host: "integracaoime.com.br"}
+  config.action_mailer.sendmail_settings = {arguments: "-i"}
   config.action_mailer.delivery_method = :sendmail
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true

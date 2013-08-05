7dde93ec5491e31e187525d001e413375db6457b
Jan Segre <jan@segre.in>
2013-08-05 00:00:57 -0300

    Adding developed_by on admin panel.

--- lib/active_admin_views_pages_base.rb
+++ lib/active_admin_views_pages_base.rb
@@ -4,6 +4,7 @@ class ActiveAdmin::Views::Pages::Base < Arbre::HTML::Document
   # Renders the content for the footer
   def build_footer
     div :id => "footer" do
+      para raw I18n.translate :developed_by
       para "&copy; #{Date.today.year.to_s} #{link_to('IntegracaoIME', root_path)}".html_safe
     end
   end
